08df0eaf8e0fc86e12873b2779adc89553b9c376
MatheoBazan <matheobazan1@gmail.com>
2026-05-29 09:53:46 -0300

    Agrego service y empiezo con AppMenu

---
+++ TP_Integrador_Programacion2/TP_Integrador_Programacion2/src/integrador/prog2/entities/Pedido.java
@@ -0,0 +1,96 @@
+package entities;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import enums.Estado;
+import enums.FormaPago;
+
+public class Pedido extends Base implements Calculable {
+
+    private LocalDate fecha;
+    private Estado estado;
+    private double total;
+    private FormaPago pago;
+    private List<DetallePedido> detalles;
+
+    public Pedido() {
+    }
+
+    public Pedido(Long id, boolean eliminado, LocalDateTime createdAt, LocalDate fecha, Estado estado, double total,
+            FormaPago pago) {
+        super(id, eliminado, createdAt);
+        this.fecha = fecha;
+        this.estado = estado;
+        this.total = total;
+        this.pago = pago;
+        this.detalles = new ArrayList<>();
+    }
+
+    public void addDetallePedido(DetallePedido detalle) {
+
+        double subtotalCalculado = detalle.getCantidad() * detalle.getProducto().getPrecio();
+
+        detalle.setSubtotal(subtotalCalculado);
+
+        this.detalles.add(detalle);
+
+        this.total = calcularTotal();
+    }
+
+    @Override
+    public double calcularTotal() {
+
+        double suma = 0;
+
+        for (DetallePedido dp : detalles) {
+
+            suma += dp.getSubtotal();
+        }
+
+        return suma;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public FormaPago getPago() {
+        return pago;
+    }
+
+    public void setPago(FormaPago pago) {
+        this.pago = pago;
+    }
+
+    public List<DetallePedido> getDetalles() {
+        return detalles;
+    }
+
+    public void setDetalles(List<DetallePedido> detalles) {
+        this.detalles = detalles;
+    }
+
+}
---
+++ TP_Integrador_Programacion2/TP_Integrador_Programacion2/src/integrador/prog2/service/ProductoService.java
@@ -0,0 +1,57 @@
+package service;
+
+import java.util.List;
+
+import dao.ProductoDAO;
+import entities.Producto;
+
+public class ProductoService implements GenericService<Producto> {
+
+    private ProductoDAO ProductoDAO = new ProductoDAO();
+
+    @Override
+    public void create(Producto entity) {
+
+        if (entity.getPrecio() < 0) {
+            System.out.println("Error: El precio no puede ser menor a 0.");
+            return;
+        }
+
+        if (entity.getNombre() == null || entity.getNombre().trim().isEmpty()) {
+            System.out.println("Error: El producto debe tener un nombre.");
+            return;
+        }
+
+        ProductoDAO.create(entity);
+    }
+
+    @Override
+    public List<Producto> readAll() {
+        return ProductoDAO.readAll();
+    }
+
+    @Override
+    public Producto readByID(Long id) {
+        return ProductoDAO.readByID(id);
+    }
+
+    @Override
+    public void update(Producto entity) {
+
+        if (entity.getPrecio() < 0) {
+            System.out.println("Error: El precio no puede ser menor a 0.");
+            return;
+        }
+
+        ProductoDAO.update(entity);
+    }
+
+    @Override
+    public void delete(Long id) {
+        ProductoDAO.delete(id);
+    }
+
+    public Producto[] ReadAll() {
+        throw new UnsupportedOperationException("Unimplemented method 'ReadAll'");
+    }
+}
